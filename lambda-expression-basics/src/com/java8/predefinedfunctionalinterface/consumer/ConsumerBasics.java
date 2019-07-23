b078846f782b58ea5a48e32689ed1ed19c0d7f8f
kunwar45 <knir45@gmail.com>
2019-07-23 15:15:08 -0400

    commit 3

---
+++ lambda-expression-basics/src/com/java8/predefinedfunctionalinterface/consumer/ConsumerBasics.java
@@ -0,0 +1,10 @@
+package com.java8.predefinedfunctionalinterface.consumer;
+
+public class ConsumerBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ lambda-expression-basics/src/com/java8/predefinedfunctionalinterface/supplier/SupplierBasics.java
@@ -0,0 +1,10 @@
+package com.java8.predefinedfunctionalinterface.supplier;
+
+public class SupplierBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
